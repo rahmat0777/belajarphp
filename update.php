423e6ffa2a464b278e43a4c967391540ee193feb
rahmat <sidikrahmat029@gmail.com>
2022-11-28 22:21:24 +0800

    selesai

---
+++ update.php
@@ -0,0 +1,32 @@
+<?php
+
+include("config.php");
+
+// cek apakah tombol simpan sudah diklik atau blum?
+if(isset($_POST['simpan'])){
+
+    // ambil data dari formulir
+    $id = $_POST['id'];
+    $nama = $_POST['nama'];
+    $posisi = $_POST['posisi'];
+    $no_punggung = $_POST['no_punggung'];
+    
+    // buat query update
+    $sql = "UPDATE daftar_pemain SET nama='$nama', posisi='$posisi', no_punggung='$no_punggung'  WHERE id=$id";
+    $query = mysqli_query($db, $sql);
+
+    // apakah query update berhasil?
+    if( $query ) {
+        // kalau berhasil alihkan ke halaman list-siswa.php
+        header('Location: list_pemain.php');
+    } else {
+        // kalau gagal tampilkan pesan
+        die("Gagal menyimpan perubahan...");
+    }
+
+
+} else {
+    die("Akses dilarang...");
+}
+
+?>
